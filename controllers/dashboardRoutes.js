d3d534164c3a98391c49b640588b007a486eed06
Rhandom1 <73659834+Rhandom1@users.noreply.github.com>
2021-10-26 10:38:47 -0500

    Rewriting routes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -2,23 +2,37 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 const withAuth = require("../utils/auth");
+//For the Dashboard page
+  //render posts of the user
+router.get('/', (req, res) => {
+  // console.log(req.session);
 
-router.get("/", withAuth, (req, res) => {
   Post.findAll({
-    where: {
-      user_id: req.session.user_id,
-    },
+    attributes: ["id", "title", "post_text", "date_created"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  }) .then((dbPostData) => {
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+    res.render("dashboard");
   })
-    .then((dbPostData) => {
-      // serialize data before passing to template
-      const posts = dbPostData.map((post) => post.get({ plain: true }));
-      res.render("dashboard", { posts, logged_in: true });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
+
 
 router.get("/edit/:id", (req, res) => {
   Post.findOne({
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,45 +1,10 @@
 const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
-const withAuth = require('../utils/auth');
 
-//render homepage if not logged in
-// router.get('/', (req, res) => {
-//   if(req.session.logged_in) {
-//     res.redirect('/dashboard');
-//     return;
-//   }
-//   res.render('/login')
-// });
 
-router.get('/', (req, res) => {
-  // console.log(req.session);
 
-  Post.findAll({
-    attributes: ["id", "title", "post_text", "date_created"],
-    include: [
-      {
-        model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id"],
-        include: {
-          model: User,
-          attributes: ["username"],
-        },
-      },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  }) .then((dbPostData) => {
-    const posts = dbPostData.map((post) => post.get({ plain: true }));
-    res.render("dashboard");
-  })
-  .catch((err) => {
-    console.log(err);
-    res.status(500).json(err);
-  });
-});
+
 
 //render homepage
       //from homepage Login/signup
@@ -48,24 +13,13 @@ router.get('/homepage', (req, res) => {
   res.render('homepage');
 });
 
-router.get('/dashboard', (req, res) => {
-  res.render('dashboard');
+//render homepage if not logged in
+router.get('/', (req, res) => {
+  if(req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+  res.render('/login')
 });
-
-router.get('/login', (req, res) => {
-  res.render('login');
-});
-
-router.get('/signup', (req, res) => {
-  res.render('signup');
-});
-
-router.get('/newPost', (req, res) => {
-  res.render('newPost');
-});
-
-
-
-
 
 module.exports = router;
