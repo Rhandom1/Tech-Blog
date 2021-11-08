fa4af313b160bde209da3b370d9fbf0eee7fd8ec
Rhandom1 <73659834+Rhandom1@users.noreply.github.com>
2021-11-07 21:06:31 -0600

    homeRoutes built, needs testing

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,19 +7,28 @@ const { Post, User, Comment } = require("../models");
 
 
 //render homepage
-      //from homepage Login/signup
-router.get('/homepage', (req, res) => {
+      //set / to render the hompage
+router.get('/', (req, res) => {
 
   res.render('homepage');
 });
 
 //render homepage if not logged in
-router.get('/', (req, res) => {
+  // if logged in redirects to the dashboard
+router.get('/login', (req, res) => {
   if(req.session.logged_in) {
     res.redirect('/dashboard');
     return;
   }
-  res.render('/login')
+  res.render('/login');
+});
+
+router.get('/signup', (req,res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+  res.render('/signup');
 });
 
 module.exports = router;
